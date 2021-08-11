bbb2b6111190725102e17e535a2dfab861ad5036
Dylan McGee <dtmcgee@syr.edu>
2021-08-11 15:09:37 -0400

    moved CSS folder into src. Added part of nav

--- react-gallery-app/src/App.js
+++ react-gallery-app/src/App.js
@@ -1,12 +1,18 @@
 // import React from "react";
 import './App.css';
+import apiKey from './config';
+import Nav from './Components/Nav';
 
+const APIKey = apiKey;
+// console.log(APIKey);
 
 function App() {
   // console.log('hello world');
-
   return (
+    <>
     <h1>Dylan</h1>
+    <Nav />
+    </>
   );
 }
 
