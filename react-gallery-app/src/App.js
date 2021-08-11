5aa4c0e9605b07471c9bc32f8a3593686b2b91ea
Dylan McGee <dtmcgee@syr.edu>
2021-08-11 17:10:33 -0400

    Added fetch capability using axios

--- react-gallery-app/src/App.js
+++ react-gallery-app/src/App.js
@@ -1,19 +1,63 @@
-// import React from "react";
-import './App.css';
-import apiKey from './config';
-import Nav from './Components/Nav';
+import React, { Component } from "react";
+import {
+  BrowserRouter,
+  Route,
+  Switch
+} from 'react-router-dom';
+import axios from 'axios';
 
+//App Components
+import Nav from './Components/Nav';
+import Photo from './Components/Photo';
+
+import apiKey from './config';
 const APIKey = apiKey;
 // console.log(APIKey);
 
-function App() {
-  // console.log('hello world');
-  return (
-    <>
-    <h1>Dylan</h1>
-    <Nav />
-    </>
-  );
+
+export default class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      photos: []
+    };
+  }
+
+  componentDidMount() {
+    this.performSearch()
+  }
+
+  performSearch = () => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${APIKey}&tags=mlb&per_page=24&page=1&format=json&nojsoncallback=1`)
+      .then(response => {
+        this.setState({
+          photos: response.data.photos.photo
+        });
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data');
+      });
+  }
+  render() {
+    // console.log('hello world');
+    console.log(this.state.photos)
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <Switch>
+            <Route path='/:topic' component={Photo} />
+          </Switch>
+          <Nav />
+          <Photo />
+            {this.state.photos.map(img => {
+              const { farm, server, secret, id } = img
+              return ( <img src={`https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`} key = {id} alt = ""/> )
+            })}
+        </div>
+      </BrowserRouter>
+    )}
+    
 }
 
-export default App;
+
